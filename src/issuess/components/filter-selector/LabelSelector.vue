6c7e504001081a907fb72efa26acb73beb9f7693
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-03-07 11:49:43 -0500

    FilterSelector
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/issuess/components/filter-selector/LabelSelector.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+</script>
+
+<template>
+  <LoaderSpinner text="" :thickness="1" size="50px" />
+
+  <q-chip color="primary" v-for="label of 10" :key="label" outline clickable>
+    Chip
+  </q-chip>
+</template>
+
+<style scoped></style>
--- src/issuess/pages/ListPage.vue
+++ src/issuess/pages/ListPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+import FilterSelector from '../components/filter-selector/FilterSelector.vue';
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
 
   <div class="row">
     <div class="col-xs-12 col-md-4">
-
+      <FilterSelector />
     </div>
 
     <div class="col-xs-12 col-md-8">
