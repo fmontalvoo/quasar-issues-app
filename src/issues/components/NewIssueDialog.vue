409df2ae39277909f2501139a7dc2ac39d0a19a6
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-04-03 11:12:48 -0500

    Agrego editor de markdown
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/issues/components/NewIssueDialog.vue
+++ src/issues/components/NewIssueDialog.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
 type position = 'top' | 'standard' | 'right' | 'bottom' | 'left'
 
 const title = ref<string>('')
@@ -15,7 +18,7 @@ const position = ref<position>('bottom')
 
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-dialog v-model="isOpen" :position="position">
+    <q-dialog v-model="isOpen" :position="position" persistent>
       <q-card style="width: 500px">
         <q-form>
           <q-linear-progress :value="1" color="primary" />
@@ -28,9 +31,11 @@ const position = ref<position>('bottom')
 
             <q-space />
 
-            <q-input class="q-mb-sm" filled v-model="title" label="Title" />
-            <q-select class="q-mb-sm" filled v-model="labels" multiple :options="[]" use-chips stack-label
+            <q-input dense class="q-mb-sm" filled v-model="title" label="Title" />
+            <q-select dense class="q-mb-sm" filled v-model="labels" multiple :options="[]" use-chips stack-label
               label="Labels" />
+
+            <MdEditor v-model="description" placeholder="# Markdown" language="en-US" />
           </q-card-section>
 
           <q-card-actions class="row" align="right">
